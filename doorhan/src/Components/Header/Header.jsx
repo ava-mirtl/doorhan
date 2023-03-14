a08b52d115d73dd28c7b8bca9b70dfb71a9d2619
ava-mirtl <ava_mirtl@mail.ru>
2023-03-14 19:40:15 +0300

    feat: first screen

--- doorhan/src/Components/Header/Header.jsx
+++ doorhan/src/Components/Header/Header.jsx
@@ -1,6 +1,14 @@
 import React from 'react';
 import styles from './Header.module.scss';
 import logo from '../../assets/images/logo.png';
+import doorhan from '../../assets/images/Rectangle31.png';
+import atm from '../../assets/icons/local_atm.png';
+import percent from '../../assets/icons/percent.png';
+import settings from '../../assets/icons/settings.png';
+import shipping from '../../assets/icons/local_shipping (1).png';
+import box from '../../assets/icons/warehouse.png';
+
+import OrangeBtn from '../OrangeBtn/OrangeBtn';
 
 
 
@@ -9,9 +17,31 @@ export default function Header() {
     <div className={styles.header}> 
     <img className={styles.logo} src={logo} alt="logo"/>
     <span className={styles.phone}>+7(495)127-05-21</span>
-    <div className={styles.form}>
+    <form className={styles.form}>
     <input type="text" className={styles.input} placeholder='Телефон'/>
-    <button className={styles.btn}>Заказать звонок</button></div>
-    </div></>
+    <button type='submit' className={styles.btn}>Заказать звонок</button></form>
+    </div>
+    <div className={styles.mainСontainer}>
+<div className={styles.imgCont}>
+  <img className={styles.img} src={doorhan} alt="Ворота" />
+</div>
+<div className={styles.content}>
+  <h1>ГАРАЖНЫЕ ВОРОТА DOORHAN</h1>
+  <div className={styles.subtitle}>ПРОИЗВОДСТВО. ДОСТАВКА. МОНТАЖ.</div>
+  <div className={styles.item}>
+    <OrangeBtn img={atm}/><span className={styles.txt}>10% предоплата.</span>
+  </div>
+  <div className={styles.item}>
+    <OrangeBtn img={percent}/><span className={styles.txt}>Рассрочка от 3 до 12 месяцев</span>
+  </div><div className={styles.item}>
+    <OrangeBtn img={settings}/><span className={styles.txt}>Ежегодное бесплатное обслуживание</span>
+  </div><div className={styles.item}>
+    <OrangeBtn img={shipping}/><span className={styles.txt}>Бесплатный выезд замерщика</span>
+  </div><div className={styles.item}>
+    <OrangeBtn img={box}/><span className={styles.txt}>Свои склады  в : Одинцово, Мытищи, Люберцы, Подольск</span>
+  </div>
+</div>
+    </div>
+  </>
   )
 }
