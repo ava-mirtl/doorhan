5c69f275109f003092e8610a18b4ddc0b3900270
Krasavceva <krasavceva.va@gmail.com>
2023-03-14 14:22:07 +0300

    feat: add styles in partnerandteam

--- doorhan/src/Components/PartnerAndTeam/PartnerAndTeam.jsx
+++ doorhan/src/Components/PartnerAndTeam/PartnerAndTeam.jsx
@@ -11,44 +11,45 @@ import team3 from "../../assets/images/team3.png"
 export default function PartnerAndTeam() {
   return (
     <div>
-      <div className={styles.title}>Doorhan преимум-партнер
-      </div>
-      <div className={styles.photos}>
-        <img src={photo1} alt="photo" />
-        <img src={photo2} alt="photo" />
-        <img src={photo3} alt="photo" />
-        <img src={photo4} alt="photo" />
-      </div>
-      <div className={styles.title}>Наша команда
-      </div>
-      <div className={styles.our__team}>
-        <div className={styles.team}>
-          <img src={team1} alt="photo" />
-          <div className={styles.team__name}>
-            <div className={styles.name}>Сергей Казачинский</div>
-            <div className={styles.profession}>Генеральный директор</div>
-          </div>
+      <div className={styles.partnerandteam}>
+        <div className={styles.title1}>Doorhan преимум-партнер</div>
+        <div className={styles.photos}>
+          <img src={photo1} alt="photo" />
+          <img src={photo2} alt="photo" />
+          <img src={photo3} alt="photo" />
+          <img src={photo4} alt="photo" />
         </div>
-        <div className={styles.team}>
-          <img src={team2} alt="photo" />
-          <div className={styles.team__name}>
-            <div className={styles.name}>Виолетта Савченко</div>
-            <div className={styles.profession}>Финансовый дирекор</div>
-          </div>
+        <div className={styles.title2}>Наша команда
         </div>
-        <div className={styles.team}>
-          <img src={team3} alt="photo" />
-          <div className={styles.team__name}>
-            <div className={styles.name}>Ренат Тугушев</div>
-            <div className={styles.profession}>Руководитель монтажников</div>
+        <div className={styles.our__team}>
+          <div className={styles.team}>
+            <img src={team1} alt="photo" />
+            <div className={styles.team__name}>
+              <div className={styles.name}>Сергей Казачинский</div>
+              <div className={styles.profession}>Генеральный директор</div>
+            </div>
           </div>
-        </div>
+          <div className={styles.team}>
+            <img src={team2} alt="photo" />
+            <div className={styles.team__name}>
+              <div className={styles.name}>Виолетта Савченко</div>
+              <div className={styles.profession}>Финансовый дирекор</div>
+            </div>
+          </div>
+          <div className={styles.team}>
+            <img src={team3} alt="photo" />
+            <div className={styles.team__name}>
+              <div className={styles.name}>Ренат Тугушев</div>
+              <div className={styles.profession}>Руководитель монтажников</div>
+            </div>
+          </div>
 
 
+
+        </div>
 
       </div>
-
-
     </div>
+
   )
 }
