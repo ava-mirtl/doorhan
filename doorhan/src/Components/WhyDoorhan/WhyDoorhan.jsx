b39ba3bf3075cac508f6dfaed8c0f7686a153a38
Krasavceva <krasavceva.va@gmail.com>
2023-03-15 19:41:35 +0300

    feat: add orange f*cking circle

--- doorhan/src/Components/WhyDoorhan/WhyDoorhan.jsx
+++ doorhan/src/Components/WhyDoorhan/WhyDoorhan.jsx
@@ -8,6 +8,7 @@ import pic2 from '../../assets/images/pic2.png';
 import pic3 from '../../assets/images/pic3.png';
 import pic4 from '../../assets/images/pic4.png';
 import bigpic from '../../assets/images/image 16.png';
+import circle from '../../assets/icons/elips.png'
 
 
 export default function WhyDoorhan() {
@@ -66,9 +67,18 @@ export default function WhyDoorhan() {
           <div className={styles.container__main2}>
             <div className={styles.container__left}>
               <p className={styles.one}>10% предоплата. Оплачиваете полную стоимость изделия и доставку по факту доставки ворот на ваш объект.</p>
+              <div className={styles.container__circlepic1}>
+                <img className={styles.circle1} src={circle} alt="picture" />
+              </div>
               <p className={styles.two}>У нас свой склад в: Одинцово, Мытищи, Люберцы, Подольск.</p>
+              <div className={styles.container__circlepic2}>
+                <img className={styles.circle2} src={circle} alt="picture" />
+              </div>
               <p className={styles.three}>Ежегодное бесплатное обслуживание (регулировка роликов/пружины,
                 настройка автоматики)</p>
+              <div className={styles.container__circlepic3}>
+                <img className={styles.circle3} src={circle} alt="picture" />
+              </div>
             </div>
             <div className={styles.container__pict}>
               <div className={styles.round}>
@@ -84,11 +94,26 @@ export default function WhyDoorhan() {
 
             <div className={styles.container__right}>
               <p className={styles.four}>При покупке ворот под ключ mini box в подарок </p>
+              <div className={styles.container__circlepic4}>
+                <img className={styles.circle4} src={circle} alt="picture" />
+              </div>
               <p className={styles.five}>Покупка ворот в рассрочку от 3 до 12 месяцев</p>
+              <div className={styles.container__circlepic5}>
+                <img className={styles.circle5} src={circle} alt="picture" />
+              </div>
               <p className={styles.six}>Квалифицированные и опытные специалисты по монтажу, которые ежегодно проходят обучение по установке ворот.</p>
+              <div className={styles.container__circlepic6}>
+                <img className={styles.circle6} src={circle} alt="picture" />
+              </div>
             </div>
           </div>
+
+          <div className={styles.container__circlepic7}>
+            <img className={styles.circle7} src={circle} alt="picture" />
+          </div>
           <p className={styles.seven}>Бесплатный выезд замерщика с образцами и раскладкой цветов по RAL</p>
+
+
         </div>
       </div>
     </div>
