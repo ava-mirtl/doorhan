fe8a95c3a0f1e3584b5d59c48faaeba7a485bfad
Krasavceva <krasavceva.va@gmail.com>
2023-03-15 16:09:47 +0300

    feat: add second block in WhyDoorhan

--- doorhan/src/Components/WhyDoorhan/WhyDoorhan.jsx
+++ doorhan/src/Components/WhyDoorhan/WhyDoorhan.jsx
@@ -7,6 +7,7 @@ import pic1 from '../../assets/images/pic1.png';
 import pic2 from '../../assets/images/pic2.png';
 import pic3 from '../../assets/images/pic3.png';
 import pic4 from '../../assets/images/pic4.png';
+import bigpic from '../../assets/images/image 16.png';
 
 
 export default function WhyDoorhan() {
@@ -55,7 +56,6 @@ export default function WhyDoorhan() {
                   <Button name="ПОЛУЧИТЬ ПОДАРОК" />
                 </form>
               </div>
-
             </div>
           </div>
         </div>
@@ -63,7 +63,32 @@ export default function WhyDoorhan() {
 
         <div className={styles.container__aboutus}>
           <div className={styles.title}>ПОЧЕМУ ВЫБИРАЮТ НАС:</div>
+          <div className={styles.container__main2}>
+            <div className={styles.container__left}>
+              <p className={styles.one}>10% предоплата. Оплачиваете полную стоимость изделия и доставку по факту доставки ворот на ваш объект.</p>
+              <p className={styles.two}>У нас свой склад в: Одинцово, Мытищи, Люберцы, Подольск.</p>
+              <p className={styles.three}>Ежегодное бесплатное обслуживание (регулировка роликов/пружины,
+                настройка автоматики)</p>
+            </div>
+            <div className={styles.container__pict}>
+              <div className={styles.round}>
+                <div className={styles.round2}>
+                  <div className={styles.round3}>
+                    <div className={styles.container__bigpic}>
+                      <img className={styles.bigpic} src={bigpic} alt="picture" />
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
 
+            <div className={styles.container__right}>
+              <p className={styles.four}>При покупке ворот под ключ mini box в подарок </p>
+              <p className={styles.five}>Покупка ворот в рассрочку от 3 до 12 месяцев</p>
+              <p className={styles.six}>Квалифицированные и опытные специалисты по монтажу, которые ежегодно проходят обучение по установке ворот.</p>
+            </div>
+          </div>
+          <p className={styles.seven}>Бесплатный выезд замерщика с образцами и раскладкой цветов по RAL</p>
         </div>
       </div>
     </div>
