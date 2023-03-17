3e9121fff984f1adaea0696c0dce30f7c72b29d7
Krasavceva <krasavceva.va@gmail.com>
2023-03-17 20:07:36 +0300

    feat: form in WhyDorhan

--- doorhan/src/Components/Button/Button.jsx
+++ doorhan/src/Components/Button/Button.jsx
@@ -2,21 +2,21 @@ import React from 'react';
 import './Button.scss';
 
 export const small = {
-    padding: "10px",
-    width: "247px",
+  padding: "10px",
+  width: "247px",
 }
 export const popupStyle = {
   marginTop: "10px",
   padding: "2vw",
   fontSize: "1.6rem",
   width: "100%"
-  }
-  
- export const howto = {
-    padding: "10px",
-    width: "15vw",
-    height: "4vw",
-    fontSize: "0.8vw"
+}
+
+export const howto = {
+  padding: "10px",
+  width: "15vw",
+  height: "4vw",
+  fontSize: "0.8vw"
 }
 
 export const why = {
@@ -33,9 +33,24 @@ export const medium = {
   fontSize: "0.7vw"
 }
 
+export const our_gar = {
+  padding: "10px",
+  width: "15vw",
+  height: "3vw",
+  fontSize: "0.8vw",
+  margin: "0.5vw"
+}
+export const our_prom = {
+  padding: "10px",
+  width: "15vw",
+  height: "3vw",
+  fontSize: "0.7vw",
+  margin: "0.5vw",
+}
 
 
-export default function Button({ name, onClick, styles}) {
+
+export default function Button({ name, onClick, styles }) {
   return (
     <div>
       <button style={styles} onClick={(e) => onClick(e)} className="btn">{name}</button>
