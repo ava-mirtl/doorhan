a9409906ec63ee38116548682afd050853c362a7
ava-mirtl <ava_mirtl@mail.ru>
2023-04-04 23:31:36 +0300

    fix: mrqz

--- src/Components/Marquiz.js
+++ src/Components/Marquiz.js
@@ -4,7 +4,10 @@ function Marquiz() {
    const Marquiz = window.Marquiz;
 
    useEffect(() => {
-     window.Marquiz ('Button', {id: '60ffeb303994f3003e888dab', buttonText: 'Пройти тест', bgColor: '#d34085', textColor: '#ffffff', rounded: true, shadow: 'rgba(211, 64, 133, 0.5)', blicked: true})}, [])
+     (function(t, p) {window.Marquiz ? Marquiz.add([t, p]) 
+      : document.addEventListener('marquizLoaded', function() {Marquiz.add([t, p])})})
+      ('Button', {id: '60ffeb303994f3003e888dab', buttonText: 'РАССЧИТАТЬ ВОРОТА', bgColor: '#d34085',
+       textColor: '#ffffff', rounded: true, shadow: 'rgba(211, 64, 133, 0.5)', blicked: true})}, [])
 
    return (
 <div data-marquiz-id="60ffeb303994f3003e888dab"></div>
