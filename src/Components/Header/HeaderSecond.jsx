6dee0f95c06e7ca6ec2933e04a292438610ba19a
ava-mirtl <ava_mirtl@mail.ru>
2023-04-07 17:31:07 +0300

    fix: date\

--- src/Components/Header/HeaderSecond.jsx
+++ src/Components/Header/HeaderSecond.jsx
@@ -100,8 +100,8 @@ export default function Header() {
         </div>
       </div>
       <div className={styles.content}>
-        <h1>ГАРАЖНЫЕ ВОРОТА СО СКИДКОЙ</h1>
-        <div className={styles.subtitle}>Оформи заявку сейчас и получи скидку 25%</div>
+        <h1>ГАРАЖНЫЕ ВОРОТА DOORHAN</h1>
+        <div className={styles.subtitle}>Бесплатная доставка до объекта</div>
         <div className={styles.item}>
           <OrangeBtn img={atm} />
           <span className={styles.txt}>10% ПРЕДОПЛАТА.</span>
